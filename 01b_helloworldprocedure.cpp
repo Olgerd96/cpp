842ee0bb43414154ffa9a7a1c92d0d871a668e25
Olgerd Golub <alyaksander.golub@intel.com>
2023-08-13 11:07:00 +0200

    01b

---
+++ 01b_helloworldprocedure.cpp
@@ -0,0 +1,15 @@
+// Print "Hello world" from variable
+#include <iostream>
+
+void print_string(std::string input_string)
+{
+    std::cout << input_string << std::endl;
+}
+
+int main()
+{
+    std::string message = "Hello world v0.1b !";
+    print_string(message);
+
+    return 0;
+}
