328dc5ba330ecacbe94f96a8d9818a86e3dbf064
Olgerd <olgerd@agolub-mobl.ger.corp.intel.com>
2023-08-13 14:23:46 +0200

    get more parameters

--- 01c_helloworldsize.cpp
+++ 01c_helloworldsize.cpp
@@ -1,21 +1,27 @@
 // Print "Hello world" stats
+#include <cstring>
 #include <iostream>
-//#include <string> 
+#include <string> 
 #include <typeinfo>
 
 void print_stats(std::string input_object)
 {
-    std::cout << "Object size:\t" << sizeof(input_object)
-            << "\nObj .size prop:\t" << input_object.size()
-            << "\nObject lengh:\t" << input_object.length()
-            << "\nObject type:\t" << typeid(input_object).name()
+    std::cout << "Object size:\t" << sizeof(input_object) << " bytes"
+            << "\nObject strlen:\t" << strlen(input_object.c_str()) << " chars"
+            << "\nObj.size() prop:\t" << input_object.size() << " unknown units"
+            << "\nObj.lengh() prop:\t" << input_object.length() << " chars"
+            << "\nObject type:\t" << typeid(input_object).name() << " (use c++filt -t to find readeble name)"
             << "\nObject value:\t" << input_object << std::endl;
 }
 
 int main()
 {
-    std::string message = "Hello world v0.1c !";
+    std::string message = "Hello world v0.1c !!!__________33";
     print_stats(message);
+    std::cout << "------------------------" << std::endl;
+    print_stats("ministr");
+    std::cout << "------------------------" << std::endl;
+    print_stats("This_____is_____very-----very-----very_____long____string__________________________________________________________________________");
 
     return 0;
 }
