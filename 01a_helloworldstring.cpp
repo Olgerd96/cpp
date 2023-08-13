363d6617f281b8d84610264cc08345a358a3091a
Olgerd Golub <alyaksander.golub@intel.com>
2023-08-13 10:59:40 +0200

    01a

---
+++ 01a_helloworldstring.cpp
@@ -0,0 +1,10 @@
+// Print "Hello world" from variable
+#include <iostream>
+
+int main()
+{
+    std::string message = "Hello world v0.1a !";
+    std::cout << message << std::endl;
+
+    return 0;
+}
