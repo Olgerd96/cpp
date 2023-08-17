04d8b8ffd4d0113bf30113e303fef1bbd7dd1ed2
Olgerd <6olgerd@gmail.com>
2023-08-17 13:37:02 +0200

    Print "Hello world" from procedure

--- 01_helloworld.cpp
+++ 01_helloworld.cpp
@@ -1,10 +1,15 @@
-// Print "Hello world" from variable
+// Print "Hello world" from procedure
 #include <iostream>
+
+void print_string(std::string input_string)
+{
+    std::cout << input_string << std::endl;
+}
 
 int main()
 {
-    std::string message = "Hello world v0.1a !";
-    std::cout << message << std::endl;
+    std::string message = "Hello world v0.1b !";
+    print_string(message);
 
     return 0;
 }
