ec671ade9d1c1d7927e2ecb34cf33370c14ff85d
Olgerd <olgerd@elitebook.hp>
2023-08-17 22:47:02 +0200

    long vectors

--- 01f_work_with_vector_int.cpp
+++ 01f_work_with_vector_int.cpp
@@ -10,7 +10,7 @@
 void print_vector(const std::vector<int> input_vector) {
   if (input_vector.size() > 0) {
     std::cout << "{";
-    for (size_t i = 0; i < input_vector.size(); i++) {
+    for (size_t i = 0; (i < input_vector.size() && i < 15); i++) {
       std::cout << input_vector[i] << ", ";
     }
     std::cout << "}" << std::endl;
@@ -37,16 +37,17 @@ void print_stats(const std::vector<int> input_object) {
 std::vector<int> vectors_sum(std::vector<int> argument1,
                              std::vector<int> argument2) {
   std::vector<int> sum_vector;
-  for (int i = 0; i < std::max(argument1.size(), argument2.size()); i++) {
+  for (size_t i = 0; i < std::max(argument1.size(), argument2.size()); i++) {
     sum_vector.push_back(argument1[i] + argument2[i]);
   }
   return sum_vector;
 }
 
 int main() {
-  const int vector_size = 10;
+  const int vector_size = 268435456;
   std::vector<int> input1(vector_size, 42);
   std::vector<int> input2(vector_size, 53);
+  std::vector<int> input_sum;
 
   for (int i = 0; i < std::max(input1.size(), input2.size()); i++) {
     if (i % 2 == 0) {
@@ -57,7 +58,8 @@ int main() {
   }
   print_vector(input1);
   print_vector(input2);
-  print_vector(vectors_sum(input1, input2));
+  input_sum = vectors_sum(input1, input2);
+  print_vector(input_sum);
 
   return 0;
 }
