1fa1c13a290b8e4e4d9ba60ea47268f517c5d2ee
Olgerd <23121127+Olgerd96@users.noreply.github.com>
2023-08-21 14:28:08 +0200

    add benchmarking

--- 01f_work_with_vector_int.cpp
+++ 01f_work_with_vector_int.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <typeinfo>
-#include <vector>
+#include <chrono>  	// std::chrono::steady_clock std::chrono::microseconds
+#include <iostream>  	// std::cout
+// #include <typeinfo> 	
+#include <vector> 	// std::vector<int>
 
 void print_vector(const std::vector<int> input_vector) {
   if (input_vector.size() > 0) {
@@ -8,7 +9,7 @@ void print_vector(const std::vector<int> input_vector) {
     for (size_t i = 0; (i < input_vector.size() && i < 25); i++) {
       std::cout << input_vector[i] << "  ";
     }
-    std::cout << "}\n---------------------------------------------" << std::endl;
+    std::cout << "}" << std::endl;
   }
 }
 
@@ -39,10 +40,12 @@ std::vector<int> vectors_sum(std::vector<int> argument1,
 }
 
 int main() {
-  const int vector_size = 268435456;
+  const int vector_size = 16777216;
   std::vector<int> input1(vector_size, 42);
   std::vector<int> input2(vector_size, 54);
   std::vector<int> result;
+  const int repeats = 8;
+  std::vector<int> perf_history;
 
   for (size_t i = 0; (i < std::max(input1.size(), input2.size())) && (i < 20); i++) {
     if (i % 2 == 0) {
@@ -51,9 +54,22 @@ int main() {
       input2[i] -= i;
     }
   }
+  std::cout << "Input1: \t";
   print_vector(input1);
+  std::cout << "Input2: \t";
   print_vector(input2);
-  result = vectors_sum(input1, input2);
+
+  for (int i = 0; i <= repeats; i++)
+  {
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    result = vectors_sum(input1, input2);
+    std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now();
+    std::chrono::microseconds::rep item = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+    perf_history.push_back(item);
+  }
+  std::cout << "Performance times: \t";
+  print_vector(perf_history);
+  std::cout << "Result: \t";
   print_vector(result);
 
   return 0;
